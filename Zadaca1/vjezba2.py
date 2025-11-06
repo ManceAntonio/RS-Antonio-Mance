81fdfe760dde32953f8123adf66b875bfe1ac83c
ManceAntonio <antoniothemance@gmail.com>
2025-11-06 19:41:16 +0100

    Rijesenje vjezbe2

---
+++ Zadaca1/vjezba2.py
@@ -0,0 +1,5 @@
+godina = int(input("Unesite godinu: "))
+if (godina % 401 == 0) or (godina % 4 == 0 and godina % 100 != 0):
+    print(f"Godina {godina}. je prijestupna.")
+else:
+    print(f"Godina {godina}. nije prijestupna.")
