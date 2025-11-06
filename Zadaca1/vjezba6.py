25c6c39c9610a4a9daf3ce5528b951a9b0483a84
ManceAntonio <antoniothemance@gmail.com>
2025-11-06 21:06:09 +0100

    Rijesenje vjezbe6

---
+++ Zadaca1/vjezba6.py
@@ -0,0 +1,49 @@
+# 1. for
+suma = 0
+for i in range(2, 101, 2):
+    suma += i
+print("Suma parnih brojeva:", suma)
+
+# 1. while
+suma = 0
+i = 1
+while i <= 100:
+    if i % 2 == 0:
+        suma += i
+    i += 1
+print("Suma parnih brojeva:", suma)
+
+# 2. for
+neparni = []
+for i in range(1, 20, 2):
+    neparni.append(i)
+neparni.reverse()
+print("Neparni brojevi:", neparni)
+
+# 2. while
+neparni = []
+broj = 1
+while len(neparni) < 10:
+    if broj % 2 == 1:
+        neparni.append(broj)
+    broj += 1
+neparni.reverse()
+print("Neparni brojevi:", neparni)
+
+# 3. for
+fib = [0, 1]
+for i in range(2, 1000):
+    novi = fib[-1] + fib[-2]
+    if novi > 1000:
+        break
+    fib.append(novi)
+print("Fibonaccijev niz:", fib)
+
+# 3. while
+fib = [0, 1]
+while True:
+    novi = fib[-1] + fib[-2]
+    if novi > 1000:
+        break
+    fib.append(novi)
+print("Fibonaccijev niz:", fib)
