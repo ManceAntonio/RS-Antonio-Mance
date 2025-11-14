cea653c7180a7263c97b67e47c7d0159bdd54896
ManceAntonio <antoniothemance@gmail.com>
2025-11-14 01:06:09 +0100

    rijesenje drugog podzadatka

---
+++ Zadaca3/Zadatak3/2.py
@@ -0,0 +1,7 @@
+rijeci = [
+    "jabuka", "pas", "knjiga", "zvijezda", "prijatelj",
+    "zvuk", "čokolada", "ples", "pjesma", "otorinolaringolog"
+]
+
+duljine_sa_slovom_a = [len(r) for r in rijeci if "a" in r]
+print(duljine_sa_slovom_a)
