c1519061e40954a599e7f7725145c605d0e3b840
ManceAntonio <antoniothemance@gmail.com>
2025-11-14 02:27:17 +0100

    Rijesenje cetvertog podzadatka

---
+++ Zadaca3/Zadatak4/4.py
@@ -0,0 +1,16 @@
+import math
+
+class Krug:
+    def __init__(self, r):
+        self.r = r
+
+    def opseg(self):
+        return 2 * math.pi * self.r
+
+    def povrsina(self):
+        return math.pi * self.r ** 2
+
+
+k = Krug(5)
+print(k.opseg())
+print(k.povrsina())
